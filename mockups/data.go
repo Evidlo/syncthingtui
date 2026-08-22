d1101dd3474796e15989cebb9c9e2e7a9532cc07
Evidlo <evan_gh@widloski.com>
2026-08-22 00:40:12 -0500

    add GPL-3+ copyright to all source

--- mockups/data.go
+++ mockups/data.go
@@ -1,3 +1,21 @@
+// syncthingtui - a terminal user interface for Syncthing
+// Copyright (C) 2026 Evan Widloski
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // Package mockups contains static, fake-data renderings of candidate TUI
 // layouts for human review (AGENTS.md stage 2). Each screen renders at a
 // given width/height so it can be checked in a virtual terminal.
